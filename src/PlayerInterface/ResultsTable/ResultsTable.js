2cd3c519935d11635c909907fe74a6d7f4138fe0
Jason <jason.ladias@gmail.com>
2020-02-13 00:16:20 +0200

    table pagination

--- src/PlayerInterface/ResultsTable/ResultsTable.js
+++ src/PlayerInterface/ResultsTable/ResultsTable.js
@@ -1,27 +1,102 @@
-import React from 'react'
-import { withStyles, makeStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
-const useStyles = makeStyles({
+const useStyles1 = makeStyles(theme => ({
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+    },
+}));
+
+function TablePaginationActions(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onChangePage } = props;
+
+    const handleFirstPageButtonClick = event => {
+        onChangePage(event, 0);
+    };
+
+    const handleBackButtonClick = event => {
+        onChangePage(event, page - 1);
+    };
+
+    const handleNextButtonClick = event => {
+        onChangePage(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = event => {
+        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <div className={classes.root}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </div>
+    );
+}
+
+
+const useStyles2 = makeStyles({
     table: {
-        minWidth: 650,
+        minWidth: 500,
     },
 });
 
-
-
-
-const ResultsTable = (props) => {
-
-    const classes = useStyles();
-
+export default function ResultsTable(props) {
+    const classes = useStyles2();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const rows = [...props.results]
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
     return (
         <TableContainer component={Paper} elevation={3}>
             <Table className={classes.table} size="small" aria-label="simple table">
@@ -35,7 +110,10 @@ const ResultsTable = (props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map(row => (
+                    {(rowsPerPage > 0
+                        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : rows
+                    ).map(row => (
                         <TableRow key={row.betID}>
                             <TableCell align="center">{row.team1}</TableCell>
                             <TableCell align="center">{row.team2}</TableCell>
@@ -44,10 +122,32 @@ const ResultsTable = (props) => {
                             <TableCell align="center">{row.won===1?'Won':'Lost'}</TableCell>
                         </TableRow>
                     ))}
+
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[10, 25, { label: 'All', value: -1 }]}
+                            colSpan={3}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: { 'aria-label': 'rows per page' },
+                                native: true,
+                            }}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
         </TableContainer>
-    )
+    );
 }
-
-export default ResultsTable
--- src/PlayerInterface/StatisticsCard/StatisticsCard.js
+++ src/PlayerInterface/StatisticsCard/StatisticsCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PieChart from 'react-minimal-pie-chart';
+import Chart from 'react-google-charts';
 import Card from '@material-ui/core/Card'
 import { makeStyles } from '@material-ui/core/styles'
 import CardContent from '@material-ui/core/CardContent'
@@ -22,20 +22,60 @@ const useStyles = makeStyles({
   }
 })
 
+const pieOptions = {
+  title: "",
+  pieHole: 0.6,
+  slices: [
+    {
+      color: "#2BB673"
+    },
+    {
+      color: "#d91e48"
+    },
+    {
+      color: "#007fad"
+    },
+    {
+      color: "#e9a227"
+    }
+  ],
+  legend: {
+    position: "bottom",
+    alignment: "center",
+    textStyle: {
+      color: "233238",
+      fontSize: 14
+    }
+  },
+  tooltip: {
+    showColorCode: true
+  },
+  chartArea: {
+    left: 0,
+    top: 10,
+    width: "100%",
+    height: "80%"
+  },
+  fontName: "Roboto"
+}
+
 const StatisticsCard = (props) => {
   const classes = useStyles()
 
   return (
     <Card className={classes.root} elevation={3}>
       <CardActionArea>
-        <PieChart
-          className={classes.pie}
-          data={[
-            { title: 'Won', value: props.percentage, color: '#32CD32'},
-            { title: 'Lost', value: 100 - props.percentage, color: '#C13C37'}
-          ]} />
+        <Chart
+          chartType="PieChart"
+          data={[["Age", "Weight"], ["Won", props.percentage], ["Lost", 100 - props.percentage]]}
+          options={pieOptions}
+          graph_id={props.text}
+          width={"100%"}
+          height={"15vw"}
+          legend_toggle
+        />
         <CardContent className={classes.title}>
-        <Typography gutterBottom variant="h5" component="h3" align="center">{props.text}</Typography>
+          <Typography gutterBottom variant="h5" component="h3" align="center">{props.text}</Typography>
         </CardContent>
       </CardActionArea>
     </Card>
